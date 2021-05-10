a31c41b3188ef630142790dd1749e06cc85203bb
Shawn Strickland <shawn.strickland@choosemylo.com>
2021-05-09 22:54:44 -0500

    update

--- getCurrentWeather/index.js
+++ getCurrentWeather/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github! And Shawn! And a final test!'),
+        body: JSON.stringify('Hello from Lambda and Github! And Shawn! And a final test! And again!'),
     };
     return response;
 };
