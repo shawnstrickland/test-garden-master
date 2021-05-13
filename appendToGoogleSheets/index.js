c55365a2b812bee6e0f3671ce6985d7b937e4bdd
Shawn Strickland <shawn.strickland@choosemylo.com>
2021-05-12 23:07:41 -0500

    test writing

--- appendToGoogleSheets/index.js
+++ appendToGoogleSheets/index.js
@@ -4,16 +4,17 @@ const sheets = google.sheets('v4');
 const s3 = new aws.S3();
 
 async function main (event) {
-  const s3Event = event.Records[0].s3
-  
-  var params = {
-    Bucket: s3Event.bucket.name, 
-    Key: s3Event.object.key + '/current.json'
-  }
-  
-  console.log(params.Bucket, params.Key)
-  
+  // Get S3 object written
   try {
+    const s3Event = event.Records[0].s3
+  
+    var params = {
+      Bucket: s3Event.bucket.name, 
+      Key: s3Event.object.key + '/current.json'
+    }
+    
+    console.log(params.Bucket, params.Key)
+
     const file = await s3
       .getObject(params)
       .promise()
@@ -25,48 +26,56 @@ async function main (event) {
     console.log(err);
   }
 
-  const authClient = await authorize();
+  let requests = [];
+  // Change the spreadsheet's title.
+  requests.push(
+      {
+          // updateSpreadsheetProperties: {
+          //     properties: {
+          //         title: 'Test New',
+          //     },
+          //     fields: 'title'
+          // }
+          // addSheet: {
+          //     properties: {
+          //         title: 'Yet Another Cool Test Create SheetFrom API And Again'
+          //     }
+          // }
+          appendCells: {
+            sheetId: 0,
+            rows: [
+              {
+                values: ['a test']
+              }
+            ]
+          }
+      }
+  );
+  // Find and replace text.
+  // requests.push({
+  // findReplace: {
+  //     find,
+  //     replacement,
+  //     allSheets: true,
+  // },
+  // });
 
-    // let requests = [];
-    // // Change the spreadsheet's title.
-    // requests.push(
-    //     {
-    //         // updateSpreadsheetProperties: {
-    //         //     properties: {
-    //         //         title: 'Test New',
-    //         //     },
-    //         //     fields: 'title'
-    //         // }
-    //         addSheet: {
-    //             properties: {
-    //                 title: 'Yet Another Cool Test Create SheetFrom API And Again'
-    //             }
-    //         }
-    //     }
-    // );
-    // // Find and replace text.
-    // // requests.push({
-    // // findReplace: {
-    // //     find,
-    // //     replacement,
-    // //     allSheets: true,
-    // // },
-    // // });
-
-  // try {
-  //   const batchUpdateRequest = {requests};
-  //   const response = (await sheets.spreadsheets.batchUpdate(
-  //       {
-  //           spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
-  //           resource: batchUpdateRequest,
-  //           auth: authClient
-  //       }
-  //   )).data;
-  //   // TODO: Change code below to process the `response` object:
-  //   console.log(JSON.stringify(response, null, 2));
-  // } catch (err) {
-  //   console.error(err);
-  // }
+  // Update spreadsheet
+  try {
+    const authClient = await authorize();
+    const batchUpdateRequest = {requests};
+    const response = (await sheets.spreadsheets.batchUpdate(
+        {
+            spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
+            resource: batchUpdateRequest,
+            auth: authClient
+        }
+    )).data;
+    // TODO: Change code below to process the `response` object:
+    console.log(JSON.stringify(response, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function authorize() {
