8fc2b1b8dd9ab7eb8689047761a7eb344ae61e06
Shawn Strickland <shawn.strickland@choosemylo.com>
2021-05-12 23:32:33 -0500

    trim()

--- appendToGoogleSheets/index.js
+++ appendToGoogleSheets/index.js
@@ -81,7 +81,7 @@ async function main (event) {
 }
 
 async function authorize() {
-  return new google.auth.JWT(process.env.GOOGLE_SHEETS_CLIENT_EMAIL, null, process.env.GOOGLE_SHEETS_PRIVATE_KEY, [
+  return new google.auth.JWT(process.env.GOOGLE_SHEETS_CLIENT_EMAIL, null, process.env.GOOGLE_SHEETS_PRIVATE_KEY.trim(), [
     "https://www.googleapis.com/auth/spreadsheets",
   ]);
 }
