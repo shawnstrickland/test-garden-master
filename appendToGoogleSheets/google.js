341b1688b727832b033c5518c2d1f4ed216d172b
Shawn Strickland <sjstrick@me.com>
2021-05-24 22:11:39 -0500

    semicolons, fix import

--- appendToGoogleSheets/google.js
+++ appendToGoogleSheets/google.js
@@ -1,9 +1,11 @@
+const { google } = require('googleapis');
+
 async function authorize() {
-    return new google.auth.JWT(process.env.GOOGLE_SHEETS_CLIENT_EMAIL, null, process.env.GOOGLE_SHEETS_PRIVATE_KEY.trim(), [
-      "https://www.googleapis.com/auth/spreadsheets",
-    ]);
-  }
+  return new google.auth.JWT(process.env.GOOGLE_SHEETS_CLIENT_EMAIL, null, process.env.GOOGLE_SHEETS_PRIVATE_KEY.trim(), [
+    "https://www.googleapis.com/auth/spreadsheets",
+  ]);
+}
 
 module.exports = {
-    authorize
+  authorize
 }
--- appendToGoogleSheets/index.js
+++ appendToGoogleSheets/index.js
@@ -1,49 +1,49 @@
 var aws = require('aws-sdk')
 const { google } = require('googleapis');
-const { authorize } = require('./google')
+const { authorize } = require('./google');
 const sheets = google.sheets('v4');
 const s3 = new aws.S3();
 
 function returnMonth(monthDigit) {
-  var month = new Array()
-  month[0] = "January"
-  month[1] = "February"
-  month[2] = "March"
-  month[3] = "April"
-  month[4] = "May"
-  month[5] = "June"
-  month[6] = "July"
-  month[7] = "August"
-  month[8] = "September"
-  month[9] = "October"
-  month[10] = "November"
-  month[11] = "December"
-  return month[monthDigit - 1]
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  return month[monthDigit - 1];
 }
 
 async function main(event) {
-  let precipitationTotal
-  let date
+  let precipitationTotal;
+  let date;
 
   // Get S3 object written
   try {
-    const s3Event = event.Records[0].s3
-    console.log(s3Event)
+    const s3Event = event.Records[0].s3;
+    console.log(s3Event);
 
     var params = {
       Bucket: s3Event.bucket.name,
       Key: s3Event.object.key
     }
 
-    let keyParts = params.Key.split('/')
-    date = `${keyParts[2]}/${keyParts[3]}/${keyParts[1]}`
+    let keyParts = params.Key.split('/');
+    date = `${keyParts[2]}/${keyParts[3]}/${keyParts[1]}`;
 
     const file = await s3
       .getObject(params)
-      .promise()
+      .promise();
 
-    const bodyAsJSON = JSON.parse(file.Body.toString('utf-8'))
-    precipitationTotal = bodyAsJSON.observations[0].imperial.precipTotal
+    const bodyAsJSON = JSON.parse(file.Body.toString('utf-8'));
+    precipitationTotal = bodyAsJSON.observations[0].imperial.precipTotal;
 
     // TODO: Write to sheet with month and year
     // if it doesn't already exist, add it, then append to new sheet
@@ -84,12 +84,6 @@ async function main(event) {
     console.log(err);
   }
 }
-
-// async function authorize() {
-//   return new google.auth.JWT(process.env.GOOGLE_SHEETS_CLIENT_EMAIL, null, process.env.GOOGLE_SHEETS_PRIVATE_KEY.trim(), [
-//     "https://www.googleapis.com/auth/spreadsheets",
-//   ]);
-// }
 
 exports.handler = async (event) => {
   let jsonBody = await main(event)
